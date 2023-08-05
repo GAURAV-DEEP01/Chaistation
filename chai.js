4ae8f9d8d2560bc938e379aeaddc59a7bde67732
Gaurav <gauravdeep8660@gmail.com>
2023-08-05 19:45:02 +0530

    searchbranch -v 1.0.2

--- chai.js
+++ chai.js
@@ -1,13 +1,15 @@
 let menuIcon = document.querySelector(".menu_icon")
+let menuwrapper = document.querySelector(".menuwrapper")
 let dropdownMenu = document.querySelector(".dropdown_menu")
 let dropdownCall = document.querySelector(".dropdown_call")
-let callBtn= document.querySelector(".call_btn")
+let callBtn = document.querySelector(".call_btn")
 let header = document.querySelector("header")
 let copyBtns = document.querySelectorAll(".copy_icon")
 let phoneNum = document.querySelectorAll(".the_number")
 let stars = document.querySelectorAll(".stars i");
 let searchTxt = document.querySelector(".searchTxt")
 let searchbtn = document.querySelector(".searchbtn")
+let searchBarWrap = document.querySelector(".wrap")
 
 let dropdownMenue_isopen = false;
 let dropdownCall_isopen = false;
@@ -23,18 +25,49 @@ menuIcon.addEventListener('click', () => {
       dropdownMenue_isopen = true;
    }
 })
+let searchUlElement = document.createElement('ul')
+searchBarWrap.appendChild(searchUlElement)
+searchUlElement.style.display = "none"
+searchUlElement.style.position = "absolute"
+searchUlElement.style.padding = "0px 5px"
+searchUlElement.setAttribute('class','list-group')
 
-searchbtn.addEventListener('click',()=>{
-   let searchToLower = searchTxt.value.toLocaleLowerCase()
-
-   let filteredList = data.menu.filter(e=>{
-      let theItemsName = e.item.toLocaleLowerCase() 
-      
-      return theItemsName.includes(searchToLower)
-   })
-   let tenEle = filteredList.slice(0,10)
-   console.log(tenEle)
+menuwrapper.addEventListener('click',()=>{
+   searchUlElement.style.display = "none"
 })
+
+let tenEle = [];
+
+searchTxt.addEventListener('input', () => {
+   searchTxt.value.trim()
+   UpdateDeleteSearch(tenEle)
+   if (searchTxt.value != "") {
+      let searchToLower = searchTxt.value.toLocaleLowerCase()
+      let filteredList = data.menu.filter(e => {
+         let theItemsName = e.item.toLocaleLowerCase()
+         return theItemsName.includes(searchToLower)
+      })
+      tenEle = filteredList.slice(0, 10)
+      displaySearchResult(tenEle)
+   }
+})
+
+function UpdateDeleteSearch(tenEle) {
+   if (tenEle.length != 0)
+      searchUlElement.textContent = ''
+}
+
+function displaySearchResult(tenEle) {
+   // UpdateDeleteSearch(tenEle)
+   searchUlElement.style.display = "block"
+   tenEle.forEach((e) => {
+      let listElement = document.createElement('li')
+      listElement.setAttribute('class','list-group-item')
+      listElement.style.width="97vw"
+      listElement.innerText = `${e.item}\u00A0\u00A0\u00A0\u00A0:\u00A0\u00A0\u00A0₹${e.price}`
+      searchUlElement.appendChild(listElement)
+   })
+}
 
 document.addEventListener('scroll', () => {
    dropdownMenu.style.display = "none";
