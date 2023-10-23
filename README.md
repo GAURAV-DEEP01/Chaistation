c347983dfc59056590802d726c4513d15fbdc4fa
Gaurav <gauravdeep8660@gmail.com>
2023-10-23 13:42:35 +0530

    menu -v 2.0 menuDataVerified

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # The Cafe menu website  
 ## Inprogress...
-  The website (Branch -> search) <br> 
+  <br> 
 
 [Chaistation Menu](https://gaurav-deep01.github.io/simple-prototype/index.html)
-- Has inacurate menu item data 
--- menuData.js
+++ menuData.js
@@ -18,7 +18,7 @@ const data = {
       },
       {
         "item": "ELAICHI CHAI",
-        "price": 20
+        "price": 25
       },
       {
         "item": "GINGER CHAI",
@@ -42,7 +42,7 @@ const data = {
       },
       {
         "item": "GREEN CHAI WITH HONEY",
-        "price": 25
+        "price": 30
       },
       {
         "item": "CHAI STATION SPECIAL CHAI",
@@ -50,7 +50,7 @@ const data = {
       },
       {
         "item": "TANDOORI CHAI",
-        "price": 30
+        "price": 40
       },
       {
         "item": "ZAFFRANI CHAI",
@@ -58,19 +58,19 @@ const data = {
       },
       {
         "item": "TANDOORI ZAFFRANI CHAI",
-        "price": 40
+        "price": 50
       },
       {
         "item": "BLACK COFFEE",
-        "price": 50
+        "price": 15
       },
       {
         "item": "NORMAL COFFEE",
-        "price": 50
+        "price": 20
       },
       {
         "item": "TANDOORI COFFEE",
-        "price": 50
+        "price": 40
       },
       {
         "item": "BADAM MILK",
@@ -98,107 +98,111 @@ const data = {
       },
       {
         "item": "ORANGE JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "MUSAMBI JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "WATERMELON JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "PINEAPPLE JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "GRAPE JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "PAPAYA JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "MUSKMELON JUICE",
-        "price": 99
+        "price": 70
       },
       {
         "item": "MANGO JUICE(S)",
-        "price": 99
+        "price": 80
       },
       {
         "item": "MIX FRUIT JUICE",
-        "price": 99
+        "price": 80
       },
       {
         "item": "POMEGRANATE JUICE",
-        "price": 99
+        "price": 80
       },
       {
-        "item": "GANGA JAMUNA JUICE STRAWBERRY MOJITO",
-        "price": 60
+        "item": "GANGA JAMUNA JUICE",
+        "price": 80
+      },
+      {
+       "item":"STRAWBERRY MOJITO",
+       "price": 99 
       },
       {
         "item": "BLUE CURACAO MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "PASSION FRUIT MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "MANGO MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "PINEAPPLE MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "LYCHEE MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "MINT MOJITO MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "GREEN APPLE MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "WATERMELON MOJITO",
-        "price": 80
+        "price": 99
       },
       {
         "item": "KIWI MOJITO",
-        "price": 90
+        "price": 99
       },
       {
         "item": "SWEETLASSI",
-        "price": 90
+        "price": 60
       },
       {
         "item": "MANGOLASSI",
-        "price": 90
+        "price": 80
       },
       {
         "item": "STRAWBERRYLASSI",
-        "price": 90
+        "price": 80
       },
       {
         "item": "CHOCOLATELASSI",
-        "price": 90
+        "price": 80
       },
       {
         "item": "FRENCH FRIES",
-        "price": 100
+        "price": 90
       },
       {
         "item": "POTATO WEDGES",
-        "price": 100
+        "price": 90
       },
       {
         "item": "MASALA FRIES",
@@ -210,43 +214,43 @@ const data = {
       },
       {
         "item": "VEG FINGER",
-        "price": 100
+        "price": 110
       },
       {
         "item": "CHEESE FRIES",
-        "price": 100
+        "price": 110
       },
       {
         "item": "VEG NUGGETS",
-        "price": 100
+        "price": 120
       },
       {
         "item": "CHICKEN FRIES",
-        "price": 100
+        "price": 130
       },
       {
         "item": "CHICKEN NUGGETS",
-        "price": 100
+        "price": 130
       },
       {
         "item": "CHICKEN POPCORN",
-        "price": 110
+        "price": 130
       },
       {
         "item": "CHICKEN LOLLIPOP",
-        "price": 110
+        "price": 180
       },
       {
         "item": "CHICKEN WINGS",
-        "price": 120
+        "price": 160
       },
       {
         "item": "CHICKEN STRIPS",
-        "price": 120
+        "price": 180
       },
       {
         "item": "CHICKEN LOADED FRIES",
-        "price": 130
+        "price": 220
       },
       {
         "item": "MAGGI",
@@ -254,59 +258,59 @@ const data = {
       },
       {
         "item": "PLAIN MAGGIE",
-        "price": 130
+        "price": 40
       },
       {
         "item": "VEG MAGGIE",
-        "price": 140
+        "price": 60
       },
       {
         "item": "SWEET CORN MAGGIE",
-        "price": 140
+        "price": 70
       },
       {
         "item": "VEG SCHEZWAN MAGGIE",
-        "price": 140
+        "price": 70
       },
       {
         "item": "EGG MAGGIE",
-        "price": 140
+        "price": 70
       },
       {
         "item": "BULLSEYE MAGGIE",
-        "price": 150
+        "price": 70
       },
       {
         "item": "PANEER MAGGIE",
-        "price": 150
+        "price": 80
       },
       {
         "item": "CHICKEN MAGGIE",
-        "price": 160
+        "price": 80
       },
       {
         "item": "CHICKEN SCHEZWAN MAGGIE",
-        "price": 170
+        "price": 90
       },
       {
         "item": "PANEER SCHEZWAN MAGGIE",
-        "price": 290
+        "price": 100
       },
       {
         "item": "EGG POCKET CHEESE MAGGIE",
-        "price": 160
+        "price": 110
       },
       {
         "item": "EGG GHEEROAST MAGGIE",
-        "price": 180
+        "price": 120
       },
       {
         "item": "CHICKEN GHEEROAST MAGGIE",
-        "price": 180
+        "price": 130
       },
       {
         "item": "PANEER GHEEROAST MAGGIE",
-        "price": 180
+        "price": 130
       },
       {
         "item": "VEGBURGER",
@@ -334,23 +338,23 @@ const data = {
       },
       {
         "item": "NUGGETS BURGER",
-        "price": 130
+        "price": 140
       },
       {
         "item": "ZINGGER BURGER",
-        "price": 130
+        "price": 140
       },
       {
         "item": "DOUBLE DECKER BURGER",
-        "price": 180
-      },
-      {
-        "item": "JUMBO BURGER",
         "price": 160
       },
       {
+        "item": "JUMBO BURGER",
+        "price": 170
+      },
+      {
         "item": "TOWER BURGER",
-        "price": 180
+        "price": 290
       },
       {
         "item": "SANDWICH",
@@ -362,19 +366,19 @@ const data = {
       },
       {
         "item": "SWEET CORN SANDWICH",
-        "price": 99
+        "price": 70
       },
       {
         "item": "VEG SANDWICH",
-        "price": 99
+        "price": 70
       },
       {
         "item": "EGGS SANDWICH",
-        "price": 99
+        "price": 70
       },
       {
         "item": "BOILED EGG SANDWICH",
-        "price": 99
+        "price": 70
       },
       {
         "item": "ALOO SANDWICH",
@@ -382,179 +386,179 @@ const data = {
       },
       {
         "item": "SHAWARMA SANDWICH",
-        "price": 99
+        "price": 100
       },
       {
         "item": "MIX FRUIT SANDWICH",
-        "price": 99
+        "price": 100
       },
       {
         "item": "MUSHROOM SANDWICH",
-        "price": 99
+        "price": 110
       },
       {
         "item": "PANEER SANDWICH",
-        "price": 99
+        "price": 110
       },
       {
         "item": "TIKKA SANDWICH",
-        "price": 90
+        "price": 1101
       },
       {
         "item": "CHICKEN SANDWICH",
-        "price": 90
-      },
-      {
-        "item": "FRIED CHICKEN SANDWICH",
-        "price": 100
-      },
-      {
-        "item": "SAUSAGE SANDWICH",
-        "price": 100
-      },
-      {
-        "item": "EGG CLUB SANDWICH",
-        "price": 100
-      },
-      {
-        "item": "VEG CLUB SANDWICH",
-        "price": 100
-      },
-      {
-        "item": "CHICKEN CLUB SANDWICH",
-        "price": 100
-      },
-      {
-        "item": "SHAWARMA CLUB SANDWICH",
-        "price": 110
-      },
-      {
-        "item": "ROLL SHAWARMA",
-        "price": 120
-      },
-      {
-        "item": "SHAWARMA BURGER",
-        "price": 140
-      },
-      {
-        "item": "MEAT ROLL SHAWARMA",
-        "price": 160
-      },
-      {
-        "item": "PLATE SHAWARMA",
-        "price": 180
-      },
-      {
-        "item": "MEAT PLATE SHAWARMA",
-        "price": 180
-      },
-      {
-        "item": "VEG WRAP",
-        "price": 25
-      },
-      {
-        "item": "ARABIAN WRAP",
-        "price": 30
-      },
-      {
-        "item": "PANEER WRAP",
-        "price": 40
-      },
-      {
-        "item": "SHAWARMA WRAP",
-        "price": 50
-      },
-      {
-        "item": "NUGGETS WRAP",
-        "price": 60
-      },
-      {
-        "item": "CHILLY CHICKEN WRAP",
-        "price": 70
-      },
-      {
-        "item": "HOT DOG WRAP",
-        "price": 70
-      },
-      {
-        "item": "TIKKA WRAP",
         "price": 80
       },
       {
-        "item": "KEBAB WRAP",
-        "price": 90
-      },
-      {
-        "item": "TWISTER WRAP",
-        "price": 100
-      },
-      {
-        "item": "MIXED WRAP",
-        "price": 110
-      },
-      {
-        "item": "LIME JUICE",
-        "price": 90
-      },
-      {
-        "item": "GRAPE LIME JUICE",
-        "price": 90
-      },
-      {
-        "item": "GINGER LIME JUICE",
-        "price": 100
-      },
-      {
-        "item": "MINT LIME JUICE",
-        "price": 100
-      },
-      {
-        "item": "PINEAPPLE LIME JUICE",
-        "price": 110
-      },
-      {
-        "item": "ORANGE LIME JUICE",
-        "price": 110
-      },
-      {
-        "item": "STRAWBERRY LIME JUICE",
+        "item": "FRIED CHICKEN SANDWICH",
         "price": 120
       },
       {
-        "item": "MILK LIME JUICE",
-        "price": 130
+        "item": "SAUSAGE SANDWICH",
+        "price": 120
       },
       {
-              "item": "LIME SODA",
-        "price": 130
+        "item": "EGG CLUB SANDWICH",
+        "price": 140
       },
       {
-        "item": "BLUE LIME SODA",
-        "price": 130
-      },
-      {
-        "item": "STRAWBERRY LIME SODA",
-        "price": 130
-      },
-      {
-        "item": "CHAT MASALA SODA",
-        "price": 180
-      },
-      {
-        "item": "JALJEERA SODA",
+        "item": "VEG CLUB SANDWICH",
         "price": 160
       },
       {
-        "item": "MINT LIME SODA",
+        "item": "CHICKEN CLUB SANDWICH",
         "price": 180
       },
       {
+        "item": "SHAWARMA CLUB SANDWICH",
+        "price": 180
+      },
+      {
+        "item": "ROLL SHAWARMA",
+        "price": 60
+      },
+      {
+        "item": "SHAWARMA BURGER",
+        "price": 80
+      },
+      {
+        "item": "MEAT ROLL SHAWARMA",
+        "price": 100
+      },
+      {
+        "item": "PLATE SHAWARMA",
+        "price": 120
+      },
+      {
+        "item": "MEAT PLATE SHAWARMA",
+        "price": 150
+      },
+      {
+        "item": "VEG WRAP",
+        "price": 100
+      },
+      {
+        "item": "ARABIAN WRAP",
+        "price": 120
+      },
+      {
+        "item": "PANEER WRAP",
+        "price": 120
+      },
+      {
+        "item": "SHAWARMA WRAP",
+        "price": 120
+      },
+      {
+        "item": "NUGGETS WRAP",
+        "price": 120
+      },
+      {
+        "item": "CHILLY CHICKEN WRAP",
+        "price": 120
+      },
+      {
+        "item": "HOT DOG WRAP",
+        "price": 130
+      },
+      {
+        "item": "TIKKA WRAP",
+        "price": 130
+      },
+      {
+        "item": "KEBAB WRAP",
+        "price": 140
+      },
+      {
+        "item": "TWISTER WRAP",
+        "price": 140
+      },
+      {
+        "item": "MIXED WRAP",
+        "price": 140
+      },
+      {
+        "item": "LIME JUICE",
+        "price": 30
+      },
+      {
+        "item": "GRAPE LIME JUICE",
+        "price": 30
+      },
+      {
+        "item": "GINGER LIME JUICE",
+        "price": 40
+      },
+      {
+        "item": "MINT LIME JUICE",
+        "price": 40
+      },
+      {
+        "item": "PINEAPPLE LIME JUICE",
+        "price": 40
+      },
+      {
+        "item": "ORANGE LIME JUICE",
+        "price": 40
+      },
+      {
+        "item": "STRAWBERRY LIME JUICE",
+        "price": 40
+      },
+      {
+        "item": "MILK LIME JUICE",
+        "price": 40
+      },
+      {
+              "item": "LIME SODA",
+        "price": 40
+      },
+      {
+        "item": "BLUE LIME SODA",
+        "price": 50
+      },
+      {
+        "item": "STRAWBERRY LIME SODA",
+        "price": 50
+      },
+      {
+        "item": "CHAT MASALA SODA",
+        "price": 50
+      },
+      {
+        "item": "JALJEERA SODA",
+        "price": 50
+      },
+      {
+        "item": "MINT LIME SODA",
+        "price": 50
+      },
+      {
         "item": "KIWI LIME SODA",
-        "price": 220
+        "price": 50
       },
       {
         "item": "PINEAPPLE LIME SODA",
-        "price": 99
+        "price": 50
       },
       {
         "item": "LEMONADE KULKI",
@@ -562,43 +566,43 @@ const data = {
       },
       {
         "item": "LEMON KULKI",
-        "price": 99
+        "price": 60
       },
       {
         "item": "PINEAPPLE KULKI",
-        "price": 99
+        "price": 70
       },
       {
         "item": "MANGO KULKI",
-        "price": 99
+        "price": 70
       },
       {
         "item": "RAW MANGO KULKI",
-        "price": 99
+        "price": 70
       },
       {
         "item": "LYCHEE KULKI",
-        "price": 99
+        "price": 70
       },
       {
         "item": "KIWI KULKI",
-        "price": 99
+        "price": 60
       },
       {
         "item": "STRAWBERRY KULKI",
-        "price": 99
+        "price": 70
       },
       {
         "item": "MINT LIME KULKI",
-        "price": 99
+        "price": 70
       },
       {
         "item": "PASSION FRUIT KULKI",
-        "price": 99
+        "price": 80
       },
       {
         "item": "PINEAPPLE MANGO KULKI",
-        "price": 99
+        "price": 80
       },
       {
         "item": "combos",
@@ -631,6 +635,191 @@ const data = {
       {
         "item": "combo 7: zinger burger + lollipop (3pc) + oreo shake",
         "price": 269
-      }
+      },
+      {
+        "item": "CUCUMBER SALAD",
+        "price":60
+      },
+      {
+        "item": "TOMATO SALAD",
+        "price":60
+      },
+      {
+        "item": "GREEN SALAD",
+        "price":90
+      },
+      {
+        "item": "FRUITS SALAD",
+        "price":110
+      },
+      {
+        "item": "CHICKEN SALAD",
+        "price":150
+      },
+      {
+        "item": "BOILED EGG",
+        "price":25
+      },
+      {
+        "item": "BULLSEYE",
+        "price":30
+      },
+      {
+        "item": "TOMATO OMLET",
+        "price":50
+      },
+      {
+        "item": "BREAD OMLET",
+        "price":60
+      },
+      {
+        "item": "CHEESE OMLET",
+        "price":70
+      },
+      {
+        "item": "CHICKEN OMLET",
+        "price":70
+      },
+      {
+        "item": "PANEER OMLET",
+        "price":80
+      },
+      {
+        "item": "CCHICKEN BREAD OMLET",
+        "price":90
+      },
+      {
+        "item": "CHEESE BREAD OMLET",
+        "price":100
+      },
+      {
+        "item": "BANANASHAKE",
+        "price":80
+      },
+      {
+        "item": "OREOSHAKE",
+        "price":80
+      },
+      {
+        "item": "TENDERCOCONUTSHAKE",
+        "price":90
+      },
+      {
+        "item": "SITAPHALSHAKE",
+        "price":90
+      },
+      {
+        "item": "CHIKOOSHAKE",
+        "price":90
+      },
+      {
+        "item": "AVACADOSHAKE",
+        "price":90
+      },
+      {
+        "item": "APPLESHAKE",
+        "price":90
+      },
+      {
+        "item": "STRAWBERRYSHAKE",
+        "price":90
+      },
+      {
+        "item": "MANGOSHAKE",
+        "price":90
+      },
+      {
+        "item": "PAPAYASHAKE",
+        "price":90
+      },
+      {
+        "item": "KIWISHAKE",
+        "price":90
+      },
+      {
+        "item": "LYCHEESHAKE",
+        "price":90
+      },
+      {
+        "item": "CHOCOLATESHAKE",
+        "price":90
+      },
+      {
+        "item": "SHARJASHAKE",
+        "price":90
+      },
+      {
+        "item": "COLDCOFEE",
+        "price":90
+      },
+      {
+        "item": "ABOODSHAKE",
+        "price":100
+      },
+      {
+        "item": "DATESSHAKE",
+        "price":100
+      },
+      {
+        "item": "KITKATSHAKE",
+        "price":100
+      },
+      {
+        "item": "BOUNTYSHAKE",
+        "price":100
+      },
+      {
+        "item": "MUNCHSHAKE",
+        "price":100
+      },
+      {
+        "item": "SNICKERSSHAKE",
+        "price":100
+      },
+      {
+        "item": "ALMONDSHAKE",
+        "price":120
+      },
+      {
+        "item": "SHAKE",
+        "price":90
+      },
+      {
+        "item": "CASHEWSHAKE",
+        "price":120
+      },
+      {
+        "item": "MANGOVADOSHAKE",
+        "price":130
+      },
+      {
+        "item": "PISTASHAKE",
+        "price":130
+      },
+      {
+        "item": "MIXEDFRUITSHAKE",
+        "price":140
+      },
+      {
+        "item": "CHOCOLATE SMOOTHIE",
+        "price":99
+      },
+      {
+        "item": "BANANA MOOTHIE",
+        "price":99
+      },
+      {
+        "item": "CHERRY SMOOTHIE",
+        "price":99
+      },
+      {
+        "item": "MANGO SMOOTHIE",
+        "price":99
+      },
+      {
+        "item": "STRAWBERRY SMOOTHIE",
+        "price":99
+      },
     ]
   }
+  
